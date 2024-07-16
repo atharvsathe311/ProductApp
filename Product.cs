648769baed8caf26acfad70521b42308b0afea38
atharvsathe311 <121085747+atharvsathe311@users.noreply.github.com>
2024-07-16 23:17:01 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp
+{
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private double _discountPercentage;
+
+        public Product(int id, string name, int price, double discountPercentage)
+        {
+            _id = id;
+            _name = name;
+            _price = price;
+            _discountPercentage = discountPercentage;
+        }
+
+        public void GetAllDetails(out string productName , out int productPrice , out double discountPercentage , out double priceAfteDiscount)
+        {
+            productName = _name;
+            productPrice = _price;
+            discountPercentage = _discountPercentage;
+            priceAfteDiscount = productPrice - (productPrice * discountPercentage / 100);
+        }
+    }
+}
---
+++ ProductAppMain.cs
@@ -0,0 +1,53 @@
+﻿using ProductApp;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product[] product = new Product[]
+            {
+                new Product(1,"Shirt",2000,10),
+                new Product(2,"Trouser",1500,5)
+            };
+
+            bool mainWhileLoop = true;
+            int userProductId;
+
+            while (mainWhileLoop)
+            {
+                Console.WriteLine("Press 1 for Entering the Product Id or 2 to Exit");
+
+                int userChoice = int.Parse(Console.ReadLine());
+                switch (userChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Product Id");
+                        userProductId = int.Parse(Console.ReadLine());
+                        Product selectedProduct = product[userProductId - 1];
+
+                        selectedProduct.GetAllDetails(out string productName, out int productPriceBeforeDiscount, out double productDiscountPercentage, out double productPriceAfterDiscount);
+
+                        Console.WriteLine("The Product's Id is {0}", userProductId);
+                        Console.WriteLine("The Product's Name is {0}", productName);
+                        Console.WriteLine("The Product's Actual Price is {0}", productPriceBeforeDiscount);
+                        Console.WriteLine("The Product's Discount Percentage is {0}", productDiscountPercentage);
+                        Console.WriteLine("The Product's Price After Discount is {0}", productPriceAfterDiscount);
+                        break;
+                    case 2:
+                        Console.WriteLine("Exiting......");
+                        mainWhileLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+
+                }
+
+
+            }
+        }
+
+    }
+}
